815da28e950e01b6c7d8e23470c799124ce2f226
Thomas Veness <veness@protonmail.com>
2023-03-19 18:47:13 -0400

    Challenge 14 done

--- src/challenges/fourteen.rs
+++ src/challenges/fourteen.rs
@@ -24,7 +24,7 @@ fn oracle(prepend: &[u8], raw_input: &[u8], key: &[u8]) -> Result<Vec<u8>> {
     let secret_base_64 = "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK";
     let secret_bytes = general_purpose::STANDARD.decode(secret_base_64)?;
     let mut input = prepend.to_vec();
-    input.extend_from_slice(&raw_input);
+    input.extend_from_slice(raw_input);
     input.extend_from_slice(&secret_bytes);
     // Make sure it's padded!
     let padded = pkcs7_pad(&input, key.len());
@@ -36,23 +36,43 @@ fn oracle(prepend: &[u8], raw_input: &[u8], key: &[u8]) -> Result<Vec<u8>> {
 
 fn get_next_byte(
     prepend: &[u8],
-    inferred_prepend_l: usize,
+    inferred_l: usize,
     current_state: &[u8],
     key: &[u8],
     bs: usize,
 ) -> Result<u8> {
     let mut lookup = HashMap::new();
 
+    let extra_padding = bs - inferred_l % bs;
+    let skip_blocks = inferred_l / bs + 1;
+
     // Construct lookup table for current scenario
     // Say we know the message is "In a town"
     // Then the dangling is going to be padding this out so that
     // we know the string except for the last letter and that it matches with
     // the block size i.e.
-    // |<------16------>|
-    // |AAAAAAIn a townb|
-    //
+    // |                |<------16------>|
+    // |prefixqweAAAAAAA|AAAAAAIn a townb|
+    // Here:
+    //  inferred_prepend_l = 9
+    //  extra_padding = 7
+    //  skip_blocks = 7/16 + 1 = 1
     // So the number of padding bytes is
-    let padding_size = bs - 1 - (current_state.len() % bs);
+    let padding_size = bs - 1 - (current_state.len() % bs) + extra_padding;
+    let block = current_state.len() / bs + skip_blocks;
+
+    /*
+    println!("Inferred prepend: {}", inferred_l);
+    println!("Extra padding: {}", extra_padding);
+    println!("Sum : {}", extra_padding + inferred_l);
+    println!("Padding size : {}", padding_size);
+    println!("Message length: {}", padding_size);
+    println!("Block: {}", block);
+    println!("Skip blocks: {}", skip_blocks);
+    */
+
+    // PICK UP FROM HERE
+
     // Don't want trailing 1 from padding on the final byte
     for b in 2..255_u8 {
         // This runs from 0..=bs-1 as modulo is the same
@@ -60,8 +80,12 @@ fn get_next_byte(
         let mut padded: Vec<u8> = vec![65_u8; padding_size];
         padded.extend_from_slice(current_state);
         padded.push(b);
-        let dangling = &padded[padded.len() - bs..padded.len()];
-        let enc = oracle(prepend, dangling, key)?[..bs].to_vec();
+        // We now need to strip off the extra padding at the beginning
+        // Skip blocks is at least 1, which makes sense as we will always have a least one full
+        // block of padding
+        // Just want the block with the probes in it
+        let enc = oracle(prepend, &padded, key)?[block * bs..(block + 1) * bs].to_vec();
+
         lookup.insert(enc, b);
     }
     // Now run with slightly smaller dangling string
@@ -69,7 +93,7 @@ fn get_next_byte(
     // |<------16------>|
     // |AAAAAAIn a town?|
     // Select correct block to look at
-    let block = current_state.len() / bs;
+    // Need to include skip block now
     let enc = oracle(prepend, &padded, key)?[block * bs..(block + 1) * bs].to_vec();
 
     match lookup.get(&enc) {
@@ -134,6 +158,18 @@ pub fn main() -> Result<()> {
     println!("Actual length: {}", secret_prefix_length);
     let inferred = infer_prefix_length(&secret_prefix, &key)?;
     println!("Inferred length: {:?}", inferred);
+    // Now add extra padding on top
+    let bs = 16;
+    let mut decrypted_message: Vec<u8> =
+        Vec::with_capacity(oracle(&secret_prefix, b"", &key)?.len());
+    println!("Cracking...");
+
+    while let Ok(next_byte) = get_next_byte(&secret_prefix, inferred, &decrypted_message, &key, bs)
+    {
+        decrypted_message.push(next_byte);
+        //println!("{}", std::str::from_utf8(&decrypted_message).unwrap());
+    }
+    println!("{}", std::str::from_utf8(&decrypted_message).unwrap());
 
     Ok(())
 }
