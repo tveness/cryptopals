01e533c08d00abfb2c0bca2ea5671100c3f22e78
Tom Veness <veness@protonmail.com>
2023-04-09 19:42:12 +0100

    Challenge 53 skeleton

--- src/set7/mod.rs
+++ src/set7/mod.rs
@@ -2,6 +2,7 @@ pub mod challenge49;
 pub mod challenge50;
 pub mod challenge51;
 pub mod challenge52;
+pub mod challenge53;
 
 use crate::utils::Result;
 use anyhow::anyhow;
@@ -12,7 +13,7 @@ pub fn run(c: u64) -> Result<()> {
         50 => challenge50::main(),
         51 => challenge51::main(),
         52 => challenge52::main(),
-        //53 => challenge53::main(),
+        53 => challenge53::main(),
         //54 => challenge54::main(),
         //55 => challenge55::main(),
         //56 => challenge56::main(),
