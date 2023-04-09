42b5e0084a5af5255b830808b6041fbf6e316a86
Tom Veness <veness@protonmail.com>
2023-04-09 18:38:53 +0100

    Challenge 52

--- README.md
+++ README.md
@@ -88,7 +88,7 @@ A place to work through the CryptoPals challenges in Rust to hopefully learn thi
   - ✅ 49. CBC-MAC Message Forgery [src](src/set7/challenge49.rs)
   - ✅ 50. Hashing with CBC-MAC [src](src/set7/challenge50.rs)
   - ✅ 51. Compression Ratio Side-Channel Attacks [src](src/set7/challenge51.rs)
-  - [ ] 52. Iterated Hash Function Multicollisions
+  - ✅ 52. Iterated Hash Function Multicollisions
   - [ ] 53. Kelsey and Schneier's Expandable Messages
   - [ ] 54. Kelsey and Kohno's Nostradamus Attack
   - [ ] 55. MD4 Collisions
--- src/set7/challenge52.rs
+++ src/set7/challenge52.rs
@@ -69,27 +69,29 @@ use crate::utils::*;
 use openssl::symm::{Cipher, Crypter, Mode};
 use rand::{thread_rng, Rng};
 
+trait CrapHasher {
+    /// Updates the inner state with some data
+    fn update(&mut self, block: &[u8]);
+    /// Consumes the hasher and produces the final output
+    fn finalise(self) -> u16;
+    /// Initialiases hasher with specified state
+    fn new(hash: u16) -> Self;
+}
+
 // Crap hash function
 struct Crash {
     state: u16,
 }
 
 impl Crash {
-    fn update(&mut self, block: &[u8]) {
-        let bs = 16;
-        // Pad out to correct block size
-        for chunk in block.chunks(bs) {
-            self.state = self.eat(chunk);
-        }
-    }
-
+    // Eats a single block
     fn eat(&self, chunk: &[u8]) -> u16 {
         let mut ciphertext = vec![0; 2 * 16];
-        let mut key: Vec<u8> = vec![0x00; 14];
+        let mut key: Vec<u8> = vec![0x00; 30];
         key.push(((self.state >> 8) & 0xff) as u8);
         key.push((self.state & 0xff) as u8);
 
-        let cipher = Cipher::aes_128_ecb();
+        let cipher = Cipher::chacha20();
 
         let mut encrypter = Crypter::new(cipher, Mode::Encrypt, &key, None).unwrap();
         encrypter.pad(false);
@@ -99,16 +101,24 @@ impl Crash {
         let new_state = ((ciphertext[0] as u16) << 8) + (ciphertext[1] as u16);
         new_state
     }
+}
+
+impl CrapHasher for Crash {
+    fn new(hash: u16) -> Self {
+        Self { state: hash }
+    }
+
+    fn update(&mut self, block: &[u8]) {
+        let bs = 16;
+        // Pad out to correct block size
+        for chunk in block.chunks(bs) {
+            self.state = self.eat(chunk);
+        }
+    }
 
     // Consumes self in finalising
     fn finalise(self) -> u16 {
         self.state
-    }
-}
-
-impl Crash {
-    fn from(hash: u16) -> Self {
-        Self { state: hash }
     }
 }
 
@@ -119,13 +129,65 @@ impl Default for Crash {
     }
 }
 
-fn find_collision(state: u16) -> (Vec<u8>, Vec<u8>) {
+// Slower crap hash function
+struct SlowCrash {
+    state: u16,
+}
+
+impl SlowCrash {
+    fn eat(&self, chunk: &[u8]) -> u16 {
+        let mut ciphertext = vec![0; 2 * 32];
+        let mut key: Vec<u8> = vec![0x00; 28];
+        key.push(((self.state >> 8) & 0xff) as u8);
+        key.push((self.state & 0xff) as u8);
+        key.push(((self.state >> 8) & 0xff) as u8);
+        key.push((self.state & 0xff) as u8);
+
+        let cipher = Cipher::aes_256_ecb();
+
+        let mut encrypter = Crypter::new(cipher, Mode::Encrypt, &key, None).unwrap();
+        encrypter.pad(false);
+
+        encrypter.update(chunk, &mut ciphertext).unwrap();
+
+        let new_state = ((ciphertext[0] as u16) << 8) + (ciphertext[1] as u16);
+        new_state
+    }
+}
+
+impl CrapHasher for SlowCrash {
+    fn new(hash: u16) -> Self {
+        Self { state: hash }
+    }
+
+    fn update(&mut self, block: &[u8]) {
+        let bs = 16;
+        // Pad out to correct block size
+        for chunk in block.chunks(bs) {
+            self.state = self.eat(chunk);
+        }
+    }
+
+    // Consumes self in finalising
+    fn finalise(self) -> u16 {
+        self.state
+    }
+}
+
+impl Default for SlowCrash {
+    fn default() -> Self {
+        let state = 0;
+        Self { state }
+    }
+}
+
+fn find_collision<T: CrapHasher>(state: u16) -> (Vec<u8>, Vec<u8>) {
     let mut rng = thread_rng();
     let mut map = HashMap::<u16, Vec<u8>>::new();
     // Now go through these blocks in a deterministic fashion
     loop {
         let random_block: Vec<u8> = (0..16).map(|_| rng.gen::<u8>()).collect();
-        let mut hasher = Crash::from(state);
+        let mut hasher = T::new(state);
         hasher.update(&random_block);
         let hash = hasher.finalise();
         if let Some(old) = map.get(&hash) {
@@ -138,24 +200,35 @@ fn find_collision(state: u16) -> (Vec<u8>, Vec<u8>) {
     }
 }
 
-fn hash(block: &[u8], state: u16) -> u16 {
-    let mut hasher = Crash::from(state);
+fn hash<T: CrapHasher>(block: &[u8], state: u16) -> u16 {
+    let mut hasher = T::new(state);
     hasher.update(block);
     hasher.finalise()
 }
 
-fn gen_collision_pairs(initial_state: u16, length: usize) -> Vec<(Vec<u8>, Vec<u8>)> {
+fn hash_full<T: CrapHasher>(block: &[u8], state: u16) -> u16 {
+    let mut hasher = T::new(state);
+    for chunk in block.chunks(16) {
+        hasher.update(chunk);
+    }
+    hasher.finalise()
+}
+
+fn gen_collision_pairs<T: CrapHasher>(
+    initial_state: u16,
+    length: usize,
+) -> Vec<(Vec<u8>, Vec<u8>)> {
     // Pairs of blocks
     let mut pairs: Vec<(Vec<u8>, Vec<u8>)> = vec![];
     let mut states = vec![initial_state];
     for i in 0..length {
         // Okay, now how are we going to generate collisions?
         // First, we find a collision given a particular initial state
-        let pair = find_collision(states[i]);
+        let pair = find_collision::<T>(states[i]);
 
         //println!("Pair: {:?}", pair);
 
-        let hash0 = hash(&pair.0, states[i]);
+        let hash0 = hash::<T>(&pair.0, states[i]);
         //println!("Hash0: {}", hash0);
 
         //let hash1 = hash(&pair.1, states[i]);
@@ -169,6 +242,45 @@ fn gen_collision_pairs(initial_state: u16, length: usize) -> Vec<(Vec<u8>, Vec<u
     pairs
 }
 
+fn get_bits_for_slow_collision(
+    collision_pairs: &Vec<(Vec<u8>, Vec<u8>)>,
+) -> Option<(usize, usize)> {
+    let mut map = HashMap::new();
+    let n = collision_pairs.len();
+    // Now run through each of these and determine whether there is a collision for slow_crash
+    // How many options utilise the full tree? n choices, so 2**n distinct hashes, 2**n = 1 <<<
+    // (n+1)
+    for i in 0..(1 << (n + 1)) {
+        let mut slow_hasher = SlowCrash::default();
+        for bit in 0..n {
+            match ((i >> bit) & 0x01) == 0x01 {
+                true => slow_hasher.update(&collision_pairs[bit].0),
+                false => slow_hasher.update(&collision_pairs[bit].1),
+            }
+        }
+        let h = slow_hasher.finalise();
+
+        if let Some(old) = map.get(&h) {
+            println!("Total calls: {}", i);
+            return Some((*old, i));
+        } else {
+            map.insert(h, i);
+        }
+    }
+    None
+}
+
+fn get_slow_and_fast_collision(search_size: usize) -> (Vec<(Vec<u8>, Vec<u8>)>, (usize, usize)) {
+    // Keep generating until we find a collision pair
+    loop {
+        let collision_pairs = gen_collision_pairs::<Crash>(0, search_size);
+        if let Some(bitpair) = get_bits_for_slow_collision(&collision_pairs) {
+            return (collision_pairs, bitpair);
+        }
+        println!("Had to loop");
+    }
+}
+
 pub fn main() -> Result<()> {
     let data = b"YELLOW SUBMARINE";
     let mut hasher = Crash::default();
@@ -176,10 +288,47 @@ pub fn main() -> Result<()> {
     let hash_val = hasher.finalise();
     println!("Hash: {}", hash_val);
 
-    let n = 10;
-    let collision_pairs = gen_collision_pairs(0, n);
-    println!("List of colliding pairs: {:?}", collision_pairs);
+    // New hash function is crash(x) || slowcrash(x)
+    // So, make 2**n colliding hashes
+    // Given output space is of size 2**(16) expect to have ~sqrt these before a collision is found
+    // i.e 2**8 = 256
 
+    let n = 16;
+    // Does this until a solution is found
+    let (collision_pairs, bitpair) = get_slow_and_fast_collision(n);
+
+    println!("Bitpair: {:?}", bitpair);
+    // Bitpair now determines two blocks
+    let mut a_blocks = vec![];
+    let mut b_blocks = vec![];
+
+    // Reconstruct blocks from bit-patterns
+    for bit in 0..n {
+        match ((bitpair.0 >> bit) & 0x01) == 0x01 {
+            true => a_blocks.extend_from_slice(&collision_pairs[bit].0),
+            false => a_blocks.extend_from_slice(&collision_pairs[bit].1),
+        }
+        match ((bitpair.1 >> bit) & 0x01) == 0x01 {
+            true => b_blocks.extend_from_slice(&collision_pairs[bit].0),
+            false => b_blocks.extend_from_slice(&collision_pairs[bit].1),
+        }
+    }
+
+    // Now calculate hashes for each of these
+    let regular_hash_a = hash_full::<Crash>(&a_blocks, 0);
+    let regular_hash_b = hash_full::<Crash>(&b_blocks, 0);
+    println!("Fast hash a: {}", regular_hash_a);
+    println!("Fast hash b: {}", regular_hash_b);
+
+    assert_eq!(regular_hash_a, regular_hash_b);
+
+    let slow_hash_a = hash_full::<SlowCrash>(&a_blocks, 0);
+    let slow_hash_b = hash_full::<SlowCrash>(&b_blocks, 0);
+    println!("Slow hash a: {}", slow_hash_a);
+    println!("Slow hash b: {}", slow_hash_b);
+    assert_eq!(slow_hash_a, slow_hash_b);
+
+    assert_ne!(a_blocks, b_blocks);
     Ok(())
 }
 
@@ -193,11 +342,11 @@ mod tests {
         let initial_val = rng.gen::<u16>();
 
         let n = 10;
-        let collision_pairs = gen_collision_pairs(initial_val, n);
+        let collision_pairs = gen_collision_pairs::<Crash>(initial_val, n);
 
         // Pick two random paths through the tree and verify hashes are the same
-        let mut hasher_one = Crash::from(initial_val);
-        let mut hasher_two = Crash::from(initial_val);
+        let mut hasher_one = Crash::new(initial_val);
+        let mut hasher_two = Crash::new(initial_val);
         for i in 0..n {
             match rng.gen::<bool>() {
                 true => hasher_one.update(&collision_pairs[i].1),
@@ -213,5 +362,10 @@ mod tests {
         let hash_val_two = hasher_two.finalise();
 
         assert_eq!(hash_val_one, hash_val_two);
+    }
+
+    #[test]
+    fn double_collision() {
+        main().unwrap();
     }
 }
